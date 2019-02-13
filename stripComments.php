7e7a22ac4b986f857d99a8684be1013a6ec1dd8c
silviupet <44681717+silviupet@users.noreply.github.com>
2019-02-13 14:27:57 +0200

    1

--- stripComments.php
+++ stripComments.php
@@ -4,17 +4,20 @@
 $code = file_get_contents(__DIR__."/test.php");
 
 while(($start = strpos($code, "/*")) !== false ){
-	$end = strpos($code, "*/", $start + 2);
-//	endul trebuie sa fie cautat dupa start la 2 caractere pt a se evita /*/
-	$code = substr($code, 0 , $start) . substr($code , $end +2);
-//	concatenem de la incep codului pana la start cu stringul de la end+2 to final . Sunstr - fce un substring dintr-un string
-}
+		$end = strpos($code, "*/", $start + 2);
+	//	endul trebuie sa fie cautat dupa start la 2 caractere pt a se evita /*/
+		$code = substr($code, 0 , $start) . substr($code , $end +2);
+	//	concatenem de la incep codului pana la start cu stringul de la end+2 to final . Sunstr - fce un substring dintr-un string
+		
+	}	
+	
 
 while(($start = strpos($code, "//")) !== false ){
-$end = strpos($code, "\n", $start + 2);
+		$end = strpos($code, "\n", $start + 2);
 //	endul trebuie sa fie cautat dupa start la 2 caractere pt a se evita /*/
-	$code = substr($code, 0 , $start) . substr($code , $end);
-}
+		$code = substr($code, 0 , $start) . substr($code , $end);
+		
+	}
 
 echo  $code;
 
@@ -28,4 +31,3 @@ $a = explode("\n" , trim($code));
 	}
 
 echo count($b);
-
