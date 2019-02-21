9d304cee843fdcec394d37135203d633faa07951
PHP Grupa 1 <phpgrupa1@Admins-iMac-2.local>
2019-02-21 19:16:57 +0200

    1

---
+++ errors.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_WARNING | E_NOTICE);
+set_error_handler('handleError');
+function handleError($level, $message, $file, $line){
+	
+	file_put_contents('error.log', 
+					  "$level, $message, in $file, at $line \n",
+					 FILE_APPEND
+					 );
+}
+//afiseaxa doar acele tipuri de erori
+
+$a = 1;
+//	parse error - eroare de compilare - nu merge mai departe
+$b = $a/0;
+//warning - nu f grave gravitate 2
+$c = [];
+echo $c[1];
+//Notice gravitate 3
+//ca sa nu se mai afiseze erorile se pot capta intr-un eroor heandler 
+trigger_error('e grav');
+	
+	
+	
